c0dd54dc475bd0fc052dbabbd1e4d496637b09e8
Anthony <invader-sai@mail.ru>
2024-09-08 21:35:06 +0300

    add js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ tg.MainButton.color = "FF00FF";
 let btn = document.getElementById("click");
 
 btn.addEventListener("click", function(){
-    tg.MainButton.setText('Собрано');
+    tg.MainButton.setText("Собрано");
     tg.MainButton.show();
     tg.sendData("sendTestMessage");
 });
