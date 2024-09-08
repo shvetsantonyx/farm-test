df8c47f90847fb0e6d1d0d432a50e3cf3c052be7
Anthony <invader-sai@mail.ru>
2024-09-08 21:30:44 +0300

    add js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "FF00FF";
 
-let btn = document.getElementById('click');
+let btn = document.getElementById("click");
 
 btn.addEventListener("click", function(){
     tg.MainButton.setText('Собрано');
