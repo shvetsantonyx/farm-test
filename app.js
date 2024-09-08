a0afa5c2d9cbd1a776a4d5d6641ef6a970e15012
Anthony <invader-sai@mail.ru>
2024-09-08 21:24:09 +0300

    add js

---
+++ app.js
@@ -0,0 +1,14 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "FF00FF";
+
+let btn = document.getElementById('click');
+
+btn.addEventListener("click", function(){
+    tg.MainButton.setText('Собрано');
+    tg.MainButton.show();
+    tg.sendData("sendTestMessage");
+});
